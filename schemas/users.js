61f531519070b44d003eb9ea64a14f839ffa1d9d
huitopia <huitopia27@gmail.com>
2022-09-07 00:27:32 +0900

    기본 설정 완료

---
+++ schemas/users.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+require('dotenv').config();
+
+const UserSchema = new Schema({
+  u_Idx: { type: Number, required: true, unique: true },
+  u_Id: { type: String, required: true },
+  u_Pw: { type: String, default: null },
+  u_Name: { type: String, default: null },
+  u_Phone: { type: String, default: null },
+  u_Gender: { type: Boolean, required: true },
+  u_Address1: { type: String, required: true },
+  u_Address2: { type: String, required: true },
+  u_Img: { type: String, default: null },
+  createdAt: { type: String, required: true },
+});
+
+module.exports = mongoose.model('User', UserSchema);

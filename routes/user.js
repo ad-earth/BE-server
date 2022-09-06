6797d428f626c9138ab6055b250b51d10f2ebdbd
huitopia <huitopia27@gmail.com>
2022-09-07 03:34:25 +0900

    아이디, 비밀번호 찾기 구현 완료

--- routes/user.js
+++ routes/user.js
@@ -102,4 +102,116 @@ router.post('/register', async (req, res) => {
   }
 });
 
+/** 아이디 검증 */
+const findIdSchema = Joi.object({
+  u_Name: Joi.string().required(),
+  u_Phone: Joi.string()
+    .pattern(new RegExp('^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$'))
+    .required(),
+});
+/** 아이디 찾기 */
+router.get('/find-id', async (req, res) => {
+  try {
+    const { u_Name, u_Phone } = await findIdSchema.validateAsync(req.body);
+    const userName = await User.find({ u_Name: u_Name });
+    const userPhone = await User.find({ u_Phone: u_Phone });
+    if (userName.length === 0 || userPhone.length === 0) {
+      res.status(400).send({
+        errorMessage: '존재하지 않는 회원입니다.',
+      });
+      return;
+    }
+    let data = await User.findOne({ u_Name, u_Phone }, { _id: 0, u_Id: 1 });
+
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      errorMessage: '요청한 데이터 형식이 올바르지 않습니다.',
+    });
+  }
+});
+
+/** 비밀번호 찾기 1차 검증 */
+const findPwSchema = Joi.object({
+  u_Id: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{5,10}$')).required(),
+  u_Phone: Joi.string()
+    .pattern(new RegExp('^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$'))
+    .required(),
+  u_Name: Joi.string().required(),
+});
+
+/** 비밀번호 찾기 1차 */
+router.get('/find-password', async (req, res) => {
+  try {
+    const { u_Id, u_Name, u_Phone } = await findPwSchema.validateAsync(
+      req.body,
+    );
+    const userId = await User.find({ u_Id: u_Id });
+    const userName = await User.find({ u_Name: u_Name });
+    const userPhone = await User.find({ u_Phone: u_Phone });
+    if (userId.length == 0 || userName.length == 0 || userPhone.length == 0) {
+      res.status(400).send({
+        errorMessage: '존재하지 않는 회원입니다.',
+      });
+      return;
+    }
+    let data = await User.findOne(
+      { u_Name, u_Phone, u_Id },
+      { _id: 0, u_Idx: 1 },
+    );
+    if (data == null) {
+      res.status(400).send({
+        errorMessage: '존재하지 않는 회원입니다.',
+      });
+      return;
+    }
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      errorMessage: '요청한 데이터 형식이 올바르지 않습니다.',
+    });
+  }
+});
+
+/** 비밀번호 찾기 2차 검증 */
+const pwSchema = Joi.object({
+  u_Pw: Joi.string()
+    .pattern(
+      new RegExp(
+        '^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$',
+      ),
+    )
+    .required(),
+  u_Idx: Joi.number().required(),
+});
+
+/** 비밀번호 찾기 2차 */
+router.put('/reset-password', async (req, res) => {
+  try {
+    const { u_Idx, u_Pw } = await pwSchema.validateAsync(req.body);
+    /** 존재하는 유저인가 확인 */
+    const userNo = await User.find({ u_Idx });
+    if (userNo.length === 0) {
+      res.status(400).send({
+        errorMessage: '존재하지 않는 회원입니다.',
+      });
+      return;
+    }
+    const salt = await bcrypt.genSalt();
+    const hashPw = await bcrypt.hash(u_Pw, salt);
+    await User.updateOne({ u_Idx }, { $set: { u_Pw: hashPw } });
+    res.status(201).send({
+      success: true,
+    });
+    return;
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      errorMessage: '요청한 데이터 형식이 올바르지 않습니다.',
+    });
+    return;
+  }
+});
 module.exports = router;

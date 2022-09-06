050c364708ba730fb0dbccda9078258662b4cb6c
huitopia <huitopia27@gmail.com>
2022-09-07 00:27:47 +0900

    유저 라우터, 디비 연결

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const port = 3005;
 
+/** Router */
+const userRouter = require('./routes/user');
+
+/** DB */
+const connect = require('./schemas');
+connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
 app.get('/', (req, res) => {
-  res.send('hello adEarth');
+  res.send('This is server of adEarth');
 });
+
+app.use('/users', express.urlencoded({ extended: false }), userRouter);
 
 app.listen(port, () => console.log(`http://localhost:${port}`));
 

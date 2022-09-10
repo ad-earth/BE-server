57768f013b012a3b13813f8fcbf6260f78ce8319
huitopia <huitopia27@gmail.com>
2022-09-11 02:07:24 +0900

    어드민 db 생성

---
+++ schemas/admins.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+require('dotenv').config();
+
+const AdminSchema = new Schema({
+  a_Idx: { type: Number, required: true, unique: true },
+  a_Id: { type: String, required: true },
+  a_Pw: { type: String, required: true },
+  a_Brand: { type: String, required: true },
+  a_Number: { type: String, required: true },
+  a_Phone: { type: String, default: null },
+  a_Charge: { type: Number, default: null },
+  createdAt: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Admin', AdminSchema);

db4b9ba781dd1899eb3311ebc6efd6ee10a66913
huitopia <huitopia27@gmail.com>
2022-09-11 02:05:49 +0900

    env 변수명 및 파일명 변경

---
+++ middlewares/user-middleware.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+const User = require('../schemas/users');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const jwtKey = process.env.U_TOKEN;
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+  const [tokenType, tokenValue] = (authorization || '').split(' ');
+  if (tokenType !== 'Bearer') {
+    res.status(401).send({
+      errorMessage: '로그인 후 사용 가능합니다.',
+    });
+    return;
+  }
+  try {
+    const { u_Idx } = jwt.verify(tokenValue, jwtKey);
+
+    User.findOne({ u_Idx })
+      .exec()
+      .then(user => {
+        res.locals.user = user;
+        next();
+      });
+  } catch (error) {
+    res.status(401).send({
+      errorMessage: '로그인 후 사용 가능합니다.',
+    });
+    return;
+  }
+};

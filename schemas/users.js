a0af71750fca77f654e2c381b19281c9bc984e7c
huitopia <huitopia27@gmail.com>
2022-09-11 02:06:43 +0900

    field 옵션 변경

--- schemas/users.js
+++ schemas/users.js
@@ -7,10 +7,10 @@ require('dotenv').config();
 const UserSchema = new Schema({
   u_Idx: { type: Number, required: true, unique: true },
   u_Id: { type: String, required: true },
-  u_Pw: { type: String, default: null },
+  u_Pw: { type: String, required: true },
   u_Name: { type: String, default: null },
   u_Phone: { type: String, default: null },
-  u_Gender: { type: Boolean, required: true },
+  u_Gender: { type: String, required: true },
   u_Address1: { type: String, required: true },
   u_Address2: { type: String, required: true },
   u_Img: { type: String, default: null },

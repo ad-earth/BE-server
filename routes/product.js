58893c056ff85e54aa2a116cf99e9ddfcaa881ea
huitopia <huitopia27@gmail.com>
2022-09-12 01:16:32 +0900

    상품 목록 드롭, 드롭 상품 선택, 키워드 추가 기능 구현

--- routes/product.js
+++ routes/product.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Admin = require('../schemas/admins');
 const Product = require('../schemas/products');
+const Keyword = require('../schemas/keywords');
 const auth = require('../middlewares/admin-middleware');
 const fnc = require('../functions/noCreate');
 
@@ -254,6 +255,122 @@ router.put('/status/:p_No', auth, async (req, res) => {
   } catch (error) {
     res.status(400).json({
       success: false,
+    });
+  }
+});
+
+/** 상품 목록 드롭 */
+router.get('/list', auth, async (req, res) => {
+  try {
+    /** token */
+    const { admin } = res.locals;
+    const a_Idx = admin.a_Idx;
+    const productList = await Product.find(
+      { a_Idx },
+      { _id: 0, p_No: 1, p_Name: 1 },
+    ).exec();
+    if (productList.length != 0) {
+      res.status(200).json({
+        productList,
+      });
+      return;
+    } else {
+      res.status(204).json({
+        message: '상품을 등록해주세요',
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+    });
+  }
+});
+
+/** 드롭 상품 선택 */
+/** 테스트 예정 */
+router.get('/:p_No/keywords', auth, async (req, res) => {
+  try {
+    const { p_No } = req.params;
+
+    /** token */
+    const { admin } = res.locals;
+    const a_Idx = admin.a_Idx;
+
+    let before = await Keyword.find(
+      { p_No, a_Idx },
+      { _id: 0, keyword: 1 },
+    ).exec();
+
+    res.status(200).json({
+      before,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+    });
+  }
+});
+
+/** 키워드 추가 */
+router.post('/:p_No/keywords', auth, async (req, res) => {
+  try {
+    const { p_No } = req.params;
+    const { keyword } = req.body;
+
+    /** token */
+    const { admin } = res.locals;
+    const a_Idx = admin.a_Idx;
+
+    /** 기존에 등록된 키워드가 있는지 */
+    let db = await Keyword.find({ keyword, p_No }).exec();
+
+    /** 등록된 키워드가 20개이면 등록 불가 */
+    let cnt = await Keyword.find({ p_No }).count();
+    if (cnt >= 20) {
+      res.status(400).json({
+        success: false,
+        errorMessage: '키워드 등록 불가',
+      });
+      return;
+    }
+
+    /** 현재 키워드로 등록된 키워드가 없다면 */
+    if (db.length == 0) {
+      const recentNo = await Keyword.find().sort('-k_No').limit(1);
+      let k_No = 1;
+      if (recentNo.length !== 0) {
+        k_No = recentNo[0]['k_No'] + 1;
+      }
+
+      /** 날짜 생성 */
+      const createdAt = new Date(+new Date() + 3240 * 10000)
+        .toISOString()
+        .replace('T', ' ')
+        .replace(/\..*/, '');
+
+      await Keyword.create({
+        k_No,
+        p_No,
+        keyword,
+        createdAt,
+      });
+
+      res.status(201).json({
+        success: true,
+        message: 'post success',
+      });
+      return;
+    } else {
+      res.status(400).json({
+        success: false,
+        errorMessage: '이미 등록된 키워드입니다.',
+      });
+      return;
+    }
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      errorMessage: '키워드 등록 실패',
     });
   }
 });

f2f118207635bceb757082da255d55132a125634
huitopia <huitopia27@gmail.com>
2022-09-19 04:28:41 +0900

    검색 상품 조회 기능 구현

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../schemas/products');
 const Wish = require('../schemas/wishes');
+const Keyword = require('../schemas/keywords');
 const User = require('../schemas/users');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
@@ -78,7 +79,7 @@ router.get('/products', async (req, res) => {
     let { sort, page, maxpost } = req.query;
 
     /** maxpost 수만큼 page 처리 */
-    page = Number(page, maxpost);
+    Number(page, maxpost);
     let skipCnt = 0;
     page == 1 ? (skipCnt = 0) : (skipCnt = page * maxpost - maxpost);
 
@@ -92,7 +93,7 @@ router.get('/products', async (req, res) => {
       sort = '-p_Cost';
     }
 
-    /** db 총 갯수 */
+    /** 전체 게시물 수 */
     const cnt = await Product.find({}).count();
 
     /** 게시물 */
@@ -155,6 +156,185 @@ router.get('/products', async (req, res) => {
     res.status(200).json({
       cnt,
       userLike,
+      products,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+    });
+  }
+});
+
+/** 검색 상품 조회 */
+router.get('/search', async (req, res) => {
+  try {
+    let { keyword, page, maxpost } = req.query;
+
+    /** maxpost 수만큼 page 처리 */
+    Number(page, maxpost);
+    let skipCnt = 0;
+    page == 1 ? (skipCnt = 0) : (skipCnt = page * maxpost - maxpost);
+
+    /** 키워드를 등록한 전체 게시물 수 */
+    const cnt = await Keyword.find({ keyword: keyword }).count();
+
+    /** 광고 on 상태인 게시물 수 */
+    const onCnt = await Keyword.find({
+      keyword: keyword,
+      k_Status: 'on',
+    }).count();
+
+    let adProducts = [];
+    let products = [];
+
+    if (page == 1) {
+      /** 키워드 광고를 등록한 전체 게시물 순위로 뽑음 */
+      let onKeyword = await Keyword.find(
+        {
+          keyword: keyword,
+          k_Status: 'on',
+        },
+        { _id: 0, p_No: 1, k_Level: 1 },
+      )
+        .sort('k_Level')
+        .exec();
+      for (let x = 0; x < onKeyword.length; x++) {
+        let arrProd = await Product.find(
+          { p_No: onKeyword[x].p_No },
+          {
+            _id: 0,
+            p_No: 1,
+            p_Thumbnail: 1,
+            a_Brand: 1,
+            p_Name: 1,
+            p_Cost: 1,
+            p_Sale: 1,
+            p_Discount: 1,
+            p_Option: 1,
+            p_New: 1,
+            p_Best: 1,
+            p_Soldout: 1,
+            p_Review: 1,
+            p_Like: 1,
+          },
+        ).exec();
+        let objProd = arrProd.pop();
+        if (objProd != undefined) {
+          adProducts.push(objProd);
+        }
+      }
+      maxpost = maxpost - onCnt;
+    } else {
+      maxpost = 2;
+      skipCnt = maxpost - onCnt;
+    }
+    /** 키워드는 등록했지만 광고 등록하지 않은 게시물 */
+    let offKeyword = await Keyword.find(
+      {
+        keyword: keyword,
+        k_Status: 'off',
+      },
+      { _id: 0, p_No: 1 },
+    )
+      .limit(maxpost)
+      .skip(skipCnt)
+      .exec();
+    console.log('offKeyword: ', offKeyword);
+
+    for (let y in offKeyword) {
+      let arrProd2 = await Product.find(
+        { p_No: offKeyword[y].p_No },
+        {
+          _id: 0,
+          p_No: 1,
+          p_Thumbnail: 1,
+          a_Brand: 1,
+          p_Name: 1,
+          p_Cost: 1,
+          p_Sale: 1,
+          p_Discount: 1,
+          p_Option: 1,
+          p_New: 1,
+          p_Best: 1,
+          p_Soldout: 1,
+          p_Review: 1,
+          p_Like: 1,
+        },
+      )
+        .sort('-p_Like')
+        .exec();
+
+      let objProd2 = arrProd2.pop();
+      if (objProd2 != undefined) {
+        products.push(objProd2);
+      }
+    }
+
+    /** thumbnail 가공 */
+    for (let a in adProducts) {
+      adProducts[a].p_Thumbnail = adProducts[a].p_Thumbnail.slice(0, 1);
+    }
+    for (let b in products) {
+      products[b].p_Thumbnail = products[b].p_Thumbnail.slice(0, 1);
+    }
+
+    /** like 가공 */
+    for (let c in adProducts) {
+      if (adProducts[c].p_Like > 0) {
+        adProducts[c].p_Best = true;
+      } else {
+        adProducts[c].p_Best = false;
+      }
+    }
+
+    for (let d in products) {
+      if (products[d].p_Like > 0) {
+        products[d].p_Best = true;
+      } else {
+        products[d].p_Best = false;
+      }
+    }
+    /** 게시물에 좋아요를 누른 유저 */
+    let userLike = [];
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+      /** header에 token이 없으면 */
+      userLike = [];
+    } else {
+      /** header에 token이 있으면 */
+      const [tokenType, tokenValue] = (authorization || '').split(' ');
+      const { u_Idx } = jwt.verify(tokenValue, jwtKey);
+      let idx = u_Idx;
+
+      for (let e in adProducts) {
+        let wishUser = await Wish.find({
+          u_Idx: idx,
+          p_No: adProducts[e].p_No,
+        });
+        let popNo = wishUser.pop();
+
+        if (popNo != undefined) {
+          userLike.push(popNo.p_No);
+        }
+      }
+
+      for (let f in products) {
+        let wishUser2 = await Wish.find({
+          u_Idx: idx,
+          p_No: products[f].p_No,
+        });
+        let popNo2 = wishUser2.pop();
+        if (popNo2 != undefined) {
+          userLike.push(popNo2.p_No);
+        }
+      }
+    }
+
+    res.status(200).json({
+      cnt,
+      userLike,
+      adProducts,
       products,
     });
   } catch (error) {

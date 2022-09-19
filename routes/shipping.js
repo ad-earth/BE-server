26da9ba2bd7d1ce920b93d1d6965dc13e1b2fedf
huitopia <huitopia27@gmail.com>
2022-09-20 02:08:52 +0900

    배송지 정보 삭제 기능 구현

--- routes/shipping.js
+++ routes/shipping.js
@@ -6,6 +6,7 @@ const Product = require('../schemas/products');
 const Order = require('../schemas/orders');
 const auth = require('../middlewares/user-middleware');
 
+/** 배송지 정보 수정 */
 router.put('/:d_No', auth, async (req, res) => {
   try {
     const { d_No } = req.params;
@@ -36,6 +37,37 @@ router.put('/:d_No', auth, async (req, res) => {
     }
     res.status(201).json({
       success: true,
+    });
+    return;
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+    });
+  }
+});
+
+/** 배송지 정보 삭제 */
+router.delete('/:d_No', auth, async (req, res) => {
+  try {
+    const { d_No } = req.params;
+
+    /** token */
+    const { user } = res.locals;
+    const u_Idx = user.u_Idx;
+
+    /** db.Deliveries에 해당 유저의 배송지 고유번호 있나 확인 */
+    let db = await Delivery.find({ u_Idx, d_No });
+
+    if (db.length > 0) {
+      await Delivery.deleteOne({ u_Idx, d_No });
+    } else {
+      res.status(404).json({
+        errorMessage: '잘못된 요청입니다.',
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: 'delete success',
     });
     return;
   } catch (error) {

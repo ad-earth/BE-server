de6550119fbc1fff67ab745f7e0d69a78177e391
huitopia <huitopia27@gmail.com>
2022-09-21 20:18:13 +0900

    취소 상품 조회 기능 구현

---
+++ routes/cancel.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middlewares/user-middleware');
+const CancelProd = require('../schemas/cancelProd');
+
+/** 취소 상품 조회 */
+router.get('/', auth, async (req, res) => {
+  try {
+    let { page, maxpost } = req.query;
+
+    /** maxpost 수만큼 page 처리 */
+    Number(page, maxpost);
+    let skipCnt = 0;
+    page == 1 ? (skipCnt = 0) : (skipCnt = page * maxpost - maxpost);
+
+    /** token */
+    const { user } = res.locals;
+    const u_Idx = user.u_Idx;
+    let cnt = await CancelProd.find({ u_Idx }).count();
+
+    let db = await CancelProd.find({ u_Idx }).limit(maxpost).skip(skipCnt);
+    let objList = {};
+    let cancelList = [];
+    for (let x in db) {
+      let date = db[x].createdAt;
+      objList = {
+        o_No: db[x].o_No,
+        o_Date: date.substring(0, 10),
+        o_Price: db[x].o_Price,
+        products: db[x].products,
+      };
+      cancelList.push(objList);
+    }
+    res.status(200).json({
+      cnt,
+      cancelList,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+    });
+  }
+});
+
+module.exports = router;

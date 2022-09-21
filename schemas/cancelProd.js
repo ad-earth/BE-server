f94babd218b879d844bfd4d6db57c313e58e79f8
huitopia <huitopia27@gmail.com>
2022-09-21 20:19:05 +0900

    취소 상품 생성

---
+++ schemas/cancelProd.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+require('dotenv').config();
+
+const CancelProdSchema = new Schema({
+  o_No: { type: Number, required: true, unique: true },
+  u_Idx: { type: Number, required: true },
+  products: { type: Array, required: true },
+  o_Price: { type: Number, required: true },
+  createdAt: { type: String, required: true },
+});
+
+module.exports = mongoose.model('CancelProd', CancelProdSchema);

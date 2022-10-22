71df6e36d2a20c08091cf67bd734b8850338377d
huitopia <huitopia27@gmail.com>
2022-10-22 22:17:48 +0900

    옵션 배열에서 객체로 req 받으며 반환 시 배열 객체로 반환

--- routes/product.js
+++ routes/product.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../schemas/products');
 const Keyword = require('../schemas/keywords');
+const Wish = require('../schemas/wishes');
 const auth = require('../middlewares/admin-middleware');
 
 /** 상품등록 */
 router.post('/', auth, async (req, res) => {
   try {
-    const {
+    let {
       p_Category,
       p_Thumbnail,
       p_Name,
@@ -28,6 +29,22 @@ router.post('/', auth, async (req, res) => {
 
     /** 날짜 생성 */
     const createdAt = new Date(+new Date() + 3240 * 10000).toISOString();
+
+    /** 옵션 객체 배열로 변환 */
+    let arrOption = [];
+    let arrDetail = [];
+    for (let i in p_Option) {
+      arrDetail = [
+        p_Option[i].color,
+        p_Option[i].colorCode,
+        p_Option[i].option,
+        p_Option[i].optionPrice,
+        p_Option[i].optionCnt,
+      ];
+      arrOption.push(arrDetail);
+    }
+
+    p_Option = arrOption;
 
     await Product.create({
       p_No,
@@ -124,6 +141,30 @@ router.get('/', auth, async (req, res) => {
   }
 });
 
+/** 상품 목록 드롭 */
+router.get('/list', auth, async (req, res) => {
+  try {
+    /** token */
+    const { admin } = res.locals;
+    const a_Idx = admin.a_Idx;
+    console.log('a_Idx: ', a_Idx);
+
+    let productList = await Product.find(
+      { a_Idx },
+      { _id: 0, p_No: 1, p_Name: 1 },
+    ).exec();
+
+    return res.status(200).send({
+      productList,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({
+      success: false,
+    });
+  }
+});
+
 /** 상품 노출 여부 */
 router.put('/status/:p_No', auth, async (req, res) => {
   try {
@@ -172,11 +213,68 @@ router.put('/status/:p_No', auth, async (req, res) => {
   }
 });
 
+/** 상품 상세 정보 */
+router.get('/:p_No', auth, async (req, res) => {
+  try {
+    let { p_No } = req.params;
+
+    p_No = Number(p_No);
+
+    /** token */
+    const { admin } = res.locals;
+    const a_Idx = admin.a_Idx;
+
+    let prodInfo = await Product.findOne(
+      { a_Idx, p_No },
+      {
+        _id: 0,
+        p_Category: 1,
+        p_Thumbnail: 1,
+        p_Name: 1,
+        p_Cost: 1,
+        p_Sale: 1,
+        p_Discount: 1,
+        p_Option: 1,
+        p_Desc: 1,
+        p_Content: 1,
+      },
+    ).exec();
+    // console.log(prodInfo);
+
+    // prodInfo.p_Content = prodInfo.p_Content.replace(/\\/g);
+    // console.log('prodInfo.p_Content: ', prodInfo.p_Content);
+
+    // console.log(prodInfo);
+
+    let arrOption = [];
+    let objOption = {};
+    for (let i in prodInfo.p_Option) {
+      objOption = {
+        color: prodInfo.p_Option[i][0],
+        colorCode: prodInfo.p_Option[i][1],
+        option: prodInfo.p_Option[i][2],
+        optionPrice: prodInfo.p_Option[i][3],
+        optionCnt: prodInfo.p_Option[i][4],
+      };
+      arrOption.push(objOption);
+    }
+
+    prodInfo.p_Option = arrOption;
+
+    return res.status(200).send(prodInfo);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({
+      success: false,
+    });
+  }
+});
+
 /** 상품수정 */
 router.put('/:p_No', auth, async (req, res) => {
   try {
     let { p_No } = req.params;
-    const {
+    let {
       p_Category,
       p_Thumbnail,
       p_Name,
@@ -193,6 +291,23 @@ router.put('/:p_No', auth, async (req, res) => {
     /** token */
     const { admin } = res.locals;
     const a_Idx = admin.a_Idx;
+
+    /** 옵션 객체 배열 변경 */
+    let arrDetail = [];
+    let arrOption = [];
+
+    for (let i in p_Option) {
+      arrDetail = [
+        p_Option[i].color,
+        p_Option[i].colorCode,
+        p_Option[i].option,
+        p_Option[i].optionPrice,
+        p_Option[i].optionCnt,
+      ];
+      arrOption.push(arrDetail);
+    }
+
+    p_Option = arrOption;
 
     let db = await Product.findOne({ p_No }, { _id: 0, a_Idx: 1 }).exec();
 
@@ -250,7 +365,10 @@ router.delete('/', auth, async (req, res) => {
         await Product.deleteOne({ p_No: p_No[i] });
         /** 등록 키워드 삭제 */
         await Keyword.deleteOne({ p_No: p_No[i], a_Idx });
+        /** 위시리스트 삭제 */
+        await Wish.deleteMany({ p_No });
       }
+
       return res.status(200).send({
         success: true,
       });
@@ -267,24 +385,29 @@ router.delete('/', auth, async (req, res) => {
   }
 });
 
-/** 상품 목록 드롭 */
-router.get('/list', auth, async (req, res) => {
+/** 상품 부분 수정 */
+router.put('/content/:p_No', async (req, res) => {
   try {
+    let { p_No } = req.params;
+    let { p_Content } = req.body;
+    console.log('p_Content: ', p_Content);
+    console.log('p_Content type: ', typeof p_Content);
+
+    p_No = Number(p_No);
+
     /** token */
-    const { admin } = res.locals;
-    const a_Idx = admin.a_Idx;
+    // const { admin } = res.locals;
+    // const a_Idx = admin.a_Idx;
 
-    const productList = await Product.find(
-      { a_Idx },
-      { _id: 0, p_No: 1, p_Name: 1 },
-    ).exec();
+    let result = await Product.updateOne({ p_No }, { $set: { p_Content } });
+    console.log('result: ', result);
 
-    return res.status(200).send({
-      productList,
+    res.status(201).send({
+      success: true,
     });
   } catch (error) {
     console.log(error);
-    return res.status(400).send({
+    res.status(400).send({
       success: false,
     });
   }

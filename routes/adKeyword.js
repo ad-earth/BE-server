f9455ed06742e22b4992343b85551d24d24b6be1
huitopia <huitopia27@gmail.com>
2022-10-24 04:46:28 +0900

    무조건 빈 순위에 90원 등록되는 오류로 인하여 코드 수정

--- routes/adKeyword.js
+++ routes/adKeyword.js
@@ -11,43 +11,50 @@ router.get('/', auth, async (req, res) => {
     p_No = Number(p_No);
     k_Level = Number(k_Level);
 
-    /** 내 상품 제외하고 광고중인 키워드 있는지 확인 */
+    //-- 내 상품 제외하고 광고중인 키워드 있는지 확인
     let findKeyword = await Keyword.find(
       { keyword, k_Status: true, p_No: { $ne: p_No } },
       { _id: 0, __v: 0, createdAt: 0 },
-    ).exec();
+    )
+      .sort('k_Level')
+      .exec();
+    console.log('findKeyword: ', findKeyword);
 
-    let k_Cost = 0;
-    if (findKeyword.length == 0) {
-      /** 광고중인 키워드가 없다면 90원 반환 */
-      k_Cost = 90;
+    let levelCost = 0;
+
+    if (!findKeyword.length) {
+      //-- 광고중인 키워드가 없다면 90원
+      levelCost = 90;
+      console.log('바로 등록');
     } else {
-      /** 광고중인 키워드가 있다면 */
+      //-- 광고중인 키워드가 있다면
       for (let a in findKeyword) {
-        if (findKeyword[a].k_Level == k_Level) {
-          /** 예상 순위와 같은 키워드가 있으면 +10 반환 */
-          k_Cost = findKeyword[a].k_Cost + 10;
+        if (k_Level == findKeyword[a].k_Level) {
+          //-- 예상 순위와 같은 키워드가 있으면 +10 반환
+          levelCost = findKeyword[a].k_Cost + 10;
+          console.log('예상 순위 같음');
           break;
-        } else if (k_Level == 1 && findKeyword[a].k_Level == 2) {
-          /** 예상 순위가 1위이면 한 순위 낮은 광고 확인 후 입찰가 +10 */
-          k_Cost = findKeyword[a].k_Cost + 10;
+        } else if (k_Level <= 3 && k_Level + 1 == findKeyword[a].k_Level) {
+          //-- 한 순위 높은 광고 있나 확인 후 그 순위 입찰가 -10 반환
+          levelCost = findKeyword[a].k_Cost + 10;
+          console.log('1~3위 1순위 내렸을 때 결과 존재하면 그 결과의 +10 반환');
           break;
-        } else if (
-          k_Level != 1 &&
-          k_Level != 4 &&
-          k_Level - 1 == findKeyword[a].k_Level
-        ) {
-          /** 한 순위 높은 광고 있나 확인 후 그 순위 입찰가 -10 반환 */
-          k_Cost = findKeyword[a].k_Cost - 10;
+        } else if (k_Level <= 2 && k_Level + 2 == findKeyword[a].k_Level) {
+          levelCost = findKeyword[a].k_Cost + 10;
+          console.log('1~2위 2순위 내렸을 때 결과 존재하면 그 결과의 +10 반환');
+          break;
+        } else if (k_Level <= 1 && k_Level + 3 == findKeyword[a].k_Level) {
+          levelCost = findKeyword[a].k_Cost + 10;
+          console.log('1위 3순위 내렸을 때 결과 존재하면 그 결과의 +10 반환');
           break;
         } else {
-          /** 예상 순위가 4위이면 90원 반환 */
-          k_Cost = 90;
-          continue;
+          levelCost = 90;
+          console.log('제발 어느 경우든 4위에만 걸려라 제발');
         }
       }
     }
-    let levelCost = k_Cost;
+
+    console.log('levelCost: ', levelCost);
 
     return res.status(200).send({
       levelCost,

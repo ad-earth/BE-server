d67214ce76e0c4bf65ac699cd6147909c4d559d2
huitopia <huitopia27@gmail.com>
2023-01-04 13:56:26 +0900

    router 수정

--- app.js
+++ app.js
@@ -23,25 +23,43 @@ app.use(cors()); // dev
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
-/** Router */
-const userRouter = require('./routes/user');
+/** Router ver1 */
+// const userRouter = require('./routes/user');
+// const adminRouter = require('./routes/admin');
+// const productRouter = require('./routes/product');
+// const keywordRouter = require('./routes/keyword');
+// const mainRouter = require('./routes/main');
+// const wishRouter = require('./routes/wish');
+// const detailRouter = require('./routes/detail');
+// const cartRouter = require('./routes/cart');
+// const paymentRouter = require('./routes/payment');
+// const shippingRouter = require('./routes/shipping');
+// const reviewRouter = require('./routes/review');
+// const orderRouter = require('./routes/order');
+// const cancelRouter = require('./routes/cancel');
+// const adminOrderRouter = require('./routes/adminOrder');
+// const prodReportRouter = require('./routes/prodReport');
+// const keywordReportRouter = require('./routes/keywordReport');
+// const adminMainRouter = require('./routes/adminMain');
+// const adKeywordRouter = require('./routes/adKeyword');
+
+/** Router ver2 */
+const clientRouter = require('./routes/user');
 const adminRouter = require('./routes/admin');
 const productRouter = require('./routes/product');
 const keywordRouter = require('./routes/keyword');
-const mainRouter = require('./routes/main');
-const wishRouter = require('./routes/wish');
-const detailRouter = require('./routes/detail');
-const cartRouter = require('./routes/cart');
-const paymentRouter = require('./routes/payment');
-const shippingRouter = require('./routes/shipping');
-const reviewRouter = require('./routes/review');
-const orderRouter = require('./routes/order');
-const cancelRouter = require('./routes/cancel');
-const adminOrderRouter = require('./routes/adminOrder');
-const prodReportRouter = require('./routes/prodReport');
-const keywordReportRouter = require('./routes/keywordReport');
 const adminMainRouter = require('./routes/adminMain');
-const adKeywordRouter = require('./routes/adKeyword');
+const adminOrderRouter = require('./routes/adminOrder');
+const cancelRouter = require('./routes/cancel');
+const cartRouter = require('./routes/cart');
+const detailRouter = require('./routes/detail');
+const clientMainRouter = require('./routes/main');
+const clientOrderRouter = require('./routes/order');
+const paymentRouter = require('./routes/payment');
+const reviewRouter = require('./routes/review');
+const shippingRouter = require('./routes/shipping');
+const wishRouter = require('./routes/wish');
+const reportRouter = require('./routes/report');
 
 /** DB */
 const connect = require('./schemas');
@@ -56,7 +74,66 @@ app.get('/', (req, res) => {
   res.send('This is server of adEarth');
 });
 
-app.use('/users', express.urlencoded({ extended: false }), userRouter);
+/** ver1 */
+// app.use('/users', express.urlencoded({ extended: false }), userRouter);
+// app.use('/admins', express.urlencoded({ extended: false }), adminRouter);
+// app.use(
+//   '/admin-products',
+//   express.urlencoded({ extended: false }),
+//   productRouter,
+// );
+// app.use(
+//   '/admin-keywords',
+//   express.urlencoded({ extended: false }),
+//   keywordRouter,
+// );
+// app.use('/main', express.urlencoded({ extended: false }), mainRouter);
+// app.use('/wish-list', express.urlencoded({ extended: false }), wishRouter);
+// app.use('/products', express.urlencoded({ extended: false }), detailRouter);
+// app.use('/carts', express.urlencoded({ extended: false }), cartRouter);
+// app.use('/payment', express.urlencoded({ extended: false }), paymentRouter);
+// app.use(
+//   '/shipping-list',
+//   express.urlencoded({ extended: false }),
+//   shippingRouter,
+// );
+// app.use('/reviews', express.urlencoded({ extended: false }), reviewRouter);
+// app.use('/orders', express.urlencoded({ extended: false }), orderRouter);
+// app.use('/cancel-list', express.urlencoded({ extended: false }), cancelRouter);
+// app.use(
+//   '/order-list',
+//   express.urlencoded({ extended: false }),
+//   adminOrderRouter,
+// );
+// app.use(
+//   '/sales-report',
+//   express.urlencoded({ extended: false }),
+//   prodReportRouter,
+// );
+// app.use(
+//   '/ad-report',
+//   express.urlencoded({ extended: false }),
+//   keywordReportRouter,
+// );
+// app.use(
+//   '/admin-main',
+//   express.urlencoded({ extended: false }),
+//   adminMainRouter,
+// );
+// app.use(
+//   '/ad-keyword',
+//   express.urlencoded({ extended: false }),
+//   adKeywordRouter,
+// );
+
+/** ver2 */
+app.use(
+  '/',
+  express.urlencoded({ extended: false }),
+  keywordRouter,
+  reportRouter,
+);
+app.use('/users', express.urlencoded({ extended: false }), clientRouter);
 app.use('/admins', express.urlencoded({ extended: false }), adminRouter);
 app.use(
   '/admin-products',
@@ -64,47 +141,27 @@ app.use(
   productRouter,
 );
 app.use(
-  '/admin-keywords',
-  express.urlencoded({ extended: false }),
-  keywordRouter,
-);
-app.use('/main', express.urlencoded({ extended: false }), mainRouter);
-app.use('/wish-list', express.urlencoded({ extended: false }), wishRouter);
-app.use('/products', express.urlencoded({ extended: false }), detailRouter);
-app.use('/carts', express.urlencoded({ extended: false }), cartRouter);
-app.use('/payment', express.urlencoded({ extended: false }), paymentRouter);
-app.use(
-  '/shipping-list',
-  express.urlencoded({ extended: false }),
-  shippingRouter,
-);
-app.use('/reviews', express.urlencoded({ extended: false }), reviewRouter);
-app.use('/orders', express.urlencoded({ extended: false }), orderRouter);
-app.use('/cancel-list', express.urlencoded({ extended: false }), cancelRouter);
-app.use(
-  '/order-list',
-  express.urlencoded({ extended: false }),
-  adminOrderRouter,
-);
-app.use(
-  '/sales-report',
-  express.urlencoded({ extended: false }),
-  prodReportRouter,
-);
-app.use(
-  '/ad-report',
-  express.urlencoded({ extended: false }),
-  keywordReportRouter,
-);
-app.use(
   '/admin-main',
   express.urlencoded({ extended: false }),
   adminMainRouter,
 );
 app.use(
-  '/ad-keyword',
+  '/order-list',
   express.urlencoded({ extended: false }),
-  adKeywordRouter,
+  adminOrderRouter,
 );
+app.use('/cancel-list', express.urlencoded({ extended: false }), cancelRouter);
+app.use('/carts', express.urlencoded({ extended: false }), cartRouter);
+app.use('/products', express.urlencoded({ extended: false }), detailRouter);
+app.use('/main', express.urlencoded({ extended: false }), clientMainRouter);
+app.use('/orders', express.urlencoded({ extended: false }), clientOrderRouter);
+app.use('/payment', express.urlencoded({ extended: false }), paymentRouter);
+app.use('/reviews', express.urlencoded({ extended: false }), reviewRouter);
+app.use(
+  '/shipping-list',
+  express.urlencoded({ extended: false }),
+  shippingRouter,
+);
+app.use('/wish-list', express.urlencoded({ extended: false }), wishRouter);
 
 module.exports = app;

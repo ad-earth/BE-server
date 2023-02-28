2c508c4621139cf1569819af73be976d5ddeb5b4
huitopia <huitopia27@gmail.com>
2023-03-01 01:40:29 +0900

    get 요청 미들웨어 제거

--- routes/review.js
+++ routes/review.js
@@ -5,7 +5,7 @@ const middleware = require('../middlewares/client-middleware');
 
 router
   .post('/:p_No', middleware, reviewController.createReview) // 구매평 작성
-  .get('/:p_No', middleware, reviewController.getReviews) // 구매평 조회
+  .get('/:p_No', reviewController.getReviews) // 구매평 조회
   .put('/:r_No', middleware, reviewController.updateReview) // 구매평 수정
   .delete('/:r_No', middleware, reviewController.deleteReview); // 구매평 삭제
 

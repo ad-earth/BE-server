98e8f5db2665b236ef08fe963a6a60def1eacbb0
huitopia <huitopia27@gmail.com>
2023-03-14 02:50:11 +0900

    Refactoring - 장바구니 post 삭제 후 put, get, delete 생성

--- controllers/cart.js
+++ controllers/cart.js
@@ -1,30 +1,147 @@
 const Cart = require('../schemas/carts');
+const Product = require('../schemas/products');
 
 const cart = {
-  createCartList: async (req, res) => {
+  updateCart: async (req, res) => {
     try {
-      const { cartList } = req.body;
+      const { c_Type } = req.params;
+      const { k_No, p_No, p_Option } = req.body;
 
       // token
       const { user } = res.locals;
       const u_Idx = user.u_Idx;
 
-      // cart에 기존 db 있는지 확인
-      let db = await Cart.find({ u_Idx }).exec();
+      let findObj = {};
+      let setObj = {};
+      let createObj = { u_Idx, c_Type, p_No, p_Option, k_No };
 
-      // 장바구니 내역 있으면 삭제
-      if (db != null) {
-        await Cart.deleteOne({ u_Idx });
+      if (c_Type === 'd') {
+        findObj = { u_Idx, c_Type };
+        setObj = { p_No: p_No, p_Option: p_Option, k_No: k_No };
+      } else {
+        findObj = { u_Idx, c_Type, p_No };
+        setObj = { p_Option: p_Option, k_No: k_No };
       }
 
-      await Cart.create({ u_Idx, cartList });
-      return res.status(201).send({
+      const findCartProd = await Cart.find(findObj, { _id: 0 });
+
+      if (findCartProd.length === 0) {
+        await Cart.create(createObj);
+      } else {
+        await Cart.updateOne(findObj, { $set: setObj });
+      }
+
+      return res.status(200).send({
         success: true,
       });
     } catch (error) {
       console.log(error);
       return res.status(400).send({
-        message: '잘못된 요청입니다.',
+        success: false,
+      });
+    }
+  },
+  getCart: async (req, res) => {
+    try {
+      // token
+      const { user } = res.locals;
+      const u_Idx = user.u_Idx;
+
+      const cartType = 'c';
+
+      let findCartData = await Cart.find(
+        { u_Idx, c_Type: cartType },
+        { _id: 0, k_No: 1, p_No: 1, p_Option: 1 },
+      ).exec();
+      console.log('findCartData: ', findCartData);
+      let cartList = [];
+      let cartObj = {};
+      let o_Price = 0;
+
+      if (findCartData.length === 0) {
+        cartList = [];
+        o_Price = 0;
+      } else {
+        for (let a in findCartData) {
+          let cartProdNo = findCartData[a].p_No;
+
+          let findProdData = await Product.findOne(
+            { p_No: cartProdNo },
+            {
+              _id: 0,
+              a_Brand: 1,
+              p_No: 1,
+              p_Category: 1,
+              p_Sale: 1,
+              p_Discount: 1,
+              p_Name: 1,
+              p_Cost: 1,
+              p_Thumbnail: 1,
+            },
+          );
+
+          let p_Cnt = 0;
+          let p_Price = 0;
+
+          for (let b in findCartData[a].p_Option) {
+            p_Cnt = p_Cnt + findCartData[a].p_Option[b][4];
+            p_Price = p_Price + findCartData[a].p_Option[b][5];
+          }
+
+          cartObj = {
+            k_No: findCartData[a].k_No,
+            p_No: findCartData[a].p_No,
+            a_Brand: findProdData.a_Brand,
+            p_Thumbnail: findProdData.p_Thumbnail,
+            p_Category: findProdData.p_Category,
+            p_Cost: findProdData.p_Cost,
+            p_Sale: findProdData.p_Sale,
+            p_Discount: findProdData.p_Discount,
+            p_Name: findProdData.p_Name,
+            p_Option: findCartData[a].p_Option,
+            p_Cnt: p_Cnt,
+            p_Price: p_Price,
+          };
+
+          cartList.push(cartObj);
+        }
+
+        for (let c in cartList) {
+          o_Price = o_Price + cartList[c].p_Price;
+        }
+      }
+
+      return res.status(200).send({
+        cartList,
+        o_Price,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(400).send({
+        success: false,
+      });
+    }
+  },
+  deleteCart: async (req, res) => {
+    try {
+      const { c_Type } = req.params;
+      const { productList } = req.body;
+
+      // token
+      const { user } = res.locals;
+      const u_Idx = user.u_Idx;
+
+      for (let i in productList) {
+        productList[i];
+        await Cart.deleteOne({ u_Idx, c_Type, p_No: productList[i] });
+      }
+      return res.status(200).send({
+        success: true,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(400).send({
+        success: false,
       });
     }
   },

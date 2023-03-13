2723e541954c3f68186e5191a40cd9442a25ae92
huitopia <huitopia27@gmail.com>
2023-03-14 02:49:20 +0900

    Refactoring - 장바구니 post 삭제 후 put, get, delete 생성

--- routes/cart.js
+++ routes/cart.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const cartController = require('../controllers/cart');
 const middleware = require('../middlewares/client-middleware');
 
-router.post('/', middleware, cartController.createCartList); // 장바구니 생성
+router
+  .get('/', middleware, cartController.getCart) // 장바구니 조회
+  .put('/:c_Type', middleware, cartController.updateCart) // 장바구니 업데이트
+  .delete('/:c_Type', middleware, cartController.deleteCart); // 장바구니 삭제
 
 module.exports = router;
--- schemas/carts.js
+++ schemas/carts.js
@@ -6,7 +6,11 @@ require('dotenv').config();
 
 const CartSchema = new Schema({
   u_Idx: { type: Number, required: true },
-  cartList: { type: Array, required: true },
+  // cartList: { type: Array, required: true },
+  c_Type: { type: String, required: true },
+  p_No: { type: Number, required: true },
+  p_Option: { type: Array, default: null },
+  k_No: { type: Number, default: 0 },
 });
 
 module.exports = mongoose.model('Cart', CartSchema);

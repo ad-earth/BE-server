571323d5d1ed69305095054bfa5a0d7d9e34bfd3
huitopia <huitopia27@gmail.com>
2023-04-05 02:47:20 +0900

    Refactoring - 장바구니, 결제 요청 주소 통합

--- routes/payment.js
+++ routes/payment.js
@@ -4,7 +4,6 @@ const paymentController = require('../controllers/payment');
 const middleware = require('../middlewares/client-middleware');
 
 router
-  // .get('/', middleware, paymentController.getPaymentInfo) // 결제페이지 첫 화면
   .get('/complete', middleware, paymentController.getCompletePayment) // 결제 완료
   .get('/:c_Type', middleware, paymentController.getPayment)
   .post('/:c_Type', middleware, paymentController.createPayment); // 결제 내역 생성
